dd6fab869984c80ddf9f6884b17e9e2e652f7a5a
azmiao <2362020227@qq.com>
2025-09-19 14:01:57 +0800

    补充注释，新增灵舞和丰收宴的提醒日历

--- notice_class.py
+++ notice_class.py
@@ -6,17 +6,29 @@ from typing import Optional, List
 
 @dataclass
 class Notice:
+    # 名称
     name: str
+    # 是否启用
     enable: bool = True
+    # 活动内容
     content: Optional[str] = None
+    # 定时任务参数：日 | cron格式
     day: Optional[str] = None
+    # 定时任务参数：星期 | cron格式
     day_of_week: Optional[str] = None
+    # 定时任务参数：小时 | cron格式
     hour: Optional[str] = None
+    # 定时任务参数：分钟 | cron格式
     minute: Optional[str] = None
+    # 定时任务参数：开始日期 | 会被自动转化成 yyyy-MM-dd HH:mm:ss
     start_date: Optional[str] = None
+    # 定时任务参数：结束日志 | 会被自动转化成 yyyy-MM-dd HH:mm:ss
     end_date: Optional[str] = None
+    # 活动持续时间 | 纯数字，单位：分钟
     duration: Optional[int] = None
+    # 下次活动时间
     next_time: Optional[str] = None
+    # 奖励
     reward: List[str] = None
 
 
